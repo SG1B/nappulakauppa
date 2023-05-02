31188a04afcaf2bfb40812a8d01a79a6bb5ca8aa
sienielio <n2raro00@students.oamk.fi>
2023-05-02 13:28:38 +0300

    some extra edits

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -6,12 +6,12 @@ function LoginButton() {
 
   return (
     <div>
-      <button className='btn btn-outline-dark' onClick={() => setShowLogin(true)}>Log in</button>
+      <button className='btn btn-outline-dark' onClick={() => setShowLogin(true)}>Kirjaudu</button>
       {showLogin && (
         <div className="login-overlay">
           <div className="login-window">
             <div className="login-header">
-              <h2>Login</h2>
+              <h2>Kirjaudu sisään</h2>
               <button className="close-btn" onClick={() => setShowLogin(false)}>
                 X
               </button>
