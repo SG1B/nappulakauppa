1f66e2921c8614c1f54475e6986fd2f072cff34e
Sorakuoppa <n2hyti00@students.oamk.fi>
2023-05-04 08:06:10 +0300

    order napit oikean näköset

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -74,7 +74,7 @@ export default function Order({url, cart,removeFromCart,updateAmount,empty}) {
                     <td>
                       <input ref={inputs[index]} style={{width: '60px'}} value={product.amount} onChange={e => changeAmount(e,product,index)} />
                     </td>
-                    <td><button onClick={() => removeFromCart(product)}>Delete</button></td>
+                    <td><button className="btn btn-sm btn-outline-dark" onClick={() => removeFromCart(product)}>Delete</button></td>
                   </tr>
                 )
                 })}
